26e255069274c20d9670cd7ebbb0b316b3ba8da3
cwinters8 <clwinters8@gmail.com>
2019-09-08 13:04:11 -0600

    starting to set up components and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// style
+import './App.css';
+import './global.css';
+
+// components
+import Nav from './Components/Nav';
+import Courses from './Components/Courses';
+
+const api = 'http://localhost:5000/api';
+
+class App extends Component {
+  runFetch = (query) => {
+    fetch(`${api}/${query}`)
+      .then(response => {
+        response.json().then(data => {
+          return data;
+        });
+      });
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div id="root">
+          <div>
+            <Nav />
+            <hr />
+            <Switch>
+              {/* Default Route */}
+              <Route exact path="/" render={() => <Courses runFetch={this.runFetch}/>} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/Components/Courses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Courses = props => {
+  return (
+    <div className="bounds">
+      
+    </div>
+  );
+}
+
+export default Courses;
